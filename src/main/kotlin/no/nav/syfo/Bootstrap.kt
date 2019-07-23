86a459badfc3f0d6cab836f662a76b9c0835a489
Knut Nygaard <knutnyg@gmail.com>
2019-07-23 09:07:01 +0200

    Initial commit - Skall

---
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -0,0 +1,24 @@
+package no.nav.syfo
+
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import org.slf4j.LoggerFactory
+
+fun main(){
+    val logger = LoggerFactory.getLogger("no.nav.syfo.syfohelsenettproxy")
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/isAlive") {
+                call.respondText("I'm alive! :)")
+            }
+            get("/isReady") {
+                call.respondText("I'm ready! :)")
+            }
+        }
+    }.start(wait = true)
+}
