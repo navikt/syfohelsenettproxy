222adf7109f9322ed8f5e4de1ad0b17d36e49cb7
Knut Nygaard <knutnyg@gmail.com>
2019-07-30 14:37:27 +0200

    Refaktorerer litt

--- src/main/kotlin/no/nav/syfo/helsepersonell/HelsepersonellException.kt
+++ src/main/kotlin/no/nav/syfo/helsepersonell/HelsepersonellException.kt
@@ -3,7 +3,7 @@ package no.nav.syfo.helsepersonell
 import io.ktor.http.HttpStatusCode
 
 class HelsepersonellException(message: String?, cause: Throwable?) : RuntimeException(message, cause) {
-    val feilmelding = Feilmelding(status = HttpStatusCode.InternalServerError, message = message)
+    val feilmelding = Feilmelding(message = message)
 }
 
-data class Feilmelding(val status: HttpStatusCode, val message: String?)
+data class Feilmelding(val status: HttpStatusCode = HttpStatusCode.InternalServerError, val message: String?)
