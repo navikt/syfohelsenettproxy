049f9de5c509052b5c78d6543585ce3db460c680
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-09-13 14:18:09 +0200

    chore: fixed koin issue

--- src/test/kotlin/no/nav/syfo/fastlegeinformasjon/FastlegeinformasjonServiceTest.kt
+++ src/test/kotlin/no/nav/syfo/fastlegeinformasjon/FastlegeinformasjonServiceTest.kt
@@ -1,9 +1,5 @@
 package no.nav.syfo.fastlegeinformasjon
 
-import io.mockk.*
-
-
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.assertDoesNotThrow
@@ -11,11 +7,7 @@ import org.junit.jupiter.api.assertDoesNotThrow
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class FastlegeinformasjonServiceTest {
-
-    @BeforeEach
-    internal fun setup() {
-        clearAllMocks()
-    }
+    
 
     @Test
     fun shouldCreatePort() {
