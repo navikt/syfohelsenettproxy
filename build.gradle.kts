542d53d35f73211423935c209deca35a4fb95de9
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-10-24 11:55:50 +0200

    chore: added proper Nav-Callid

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,6 +101,7 @@ dependencies {
     implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
     implementation("io.ktor:ktor-server-call-logging:$ktorVersion")
     implementation("io.ktor:ktor-server-swagger:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-id:$ktorVersion")
 
     implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
     implementation("io.ktor:ktor-client-apache:$ktorVersion")
