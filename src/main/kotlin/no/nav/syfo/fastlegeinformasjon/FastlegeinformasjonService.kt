e1e9f909f4ed3d0872c5bf538ca1b10aea3ec104
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-10-24 10:57:53 +0200

    chore: boost timeout

--- src/main/kotlin/no/nav/syfo/fastlegeinformasjon/FastlegeinformasjonService.kt
+++ src/main/kotlin/no/nav/syfo/fastlegeinformasjon/FastlegeinformasjonService.kt
@@ -65,7 +65,7 @@ fun fastlegeinformasjonV2(
     createPort<IFlrExportOperations>(endpointUrl) {
         proxy {
             features.add(WSAddressingFeature())
-            features.add(TimeoutFeature(1000 * 60 * 7))
+            features.add(TimeoutFeature(1000 * 60 * 10))
         }
         port { withBasicAuth(serviceuserUsername, serviceuserPassword) }
     }
